531b2e2d19f33dc07f44533d9b9c915553826849
Omer <omerbulgak@gmail.com>
2019-09-06 12:15:20 +0300

    server.js and boilerplate for app implemented

---
+++ config.js/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 
+    '306794601c94bae5b1f972b370489b0abe1af21b2f96e58e582719ba53f34258',
+    SECRET: process.env.SECRET || 'd6a70645185be0be98ef9c2a8b79297b074a8bdf47ff9e175f81ac022caa5900',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
