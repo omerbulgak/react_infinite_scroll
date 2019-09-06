b9ee4ce22df4748ea50172270e837cd79cb4f9ea
Omer <omerbulgak@gmail.com>
2019-09-06 15:06:47 +0300

    App is completed

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -22,22 +22,24 @@ export class Images extends Component {
         this.setState({start: this.state.start + count});
         axios
             .get(`/api/photos?count=${count}&start=${start}`)
-            .then(res => this.setState({ images: res.data }));
+            .then(res => this.setState({ images: this.state.images.concat(res.data) }));
     }
 
     render() {
-        return <div className='images'>
-            <InfiniteScroll
-                dataLength={this.state.images.length}
-                next={this.fetchImages}
-                hasMore={true}
-                loader={<h4>Loading...</h4>}
-            >
-                { this.state.images.map(image=> (
-                    <Image key={image.id} image={image} />
-                ))}
-            </InfiniteScroll>
-        </div>;
+        return (
+            <div className='images'>
+                <InfiniteScroll
+                    dataLength={this.state.images.length}
+                    next={this.fetchImages}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                >
+                    { this.state.images.map(image=> (
+                        <Image key={image.id} image={image} />
+                    ))}
+                </InfiniteScroll>
+            </div>
+        )
     }
 }
 
